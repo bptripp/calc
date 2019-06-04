099dc6793bf41d220dd5bd10ae0d97199370371a
Bryan <bptripp@uwaterloo.ca>
2019-06-04 16:07:47 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = "CALC",
+    version = "0.1",
+    author = "Bryan Tripp",
+    author_email = "bptripp@uwaterloo.ca",
+    description = ("Convolutional architecture like cortex."),
+    license = "BSD",
+    keywords = "primate vision convolutional network",
+    url = "https://github.com/bptripp/calc",
+    packages=['calc'],
+)
+
